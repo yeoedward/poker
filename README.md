27414e2df440ff5bca092c97b21d6fada886bcb4
Eddy Yeo <edwardyeo@cmu.edu>
2013-12-01 02:36:07 -0500

    More or less done with UI. Need to integrate.

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ poker
 Online Poker Room
 
 TODO:
-HUD (using html and css?)
 
-Integrate UI with networking
+    Integrate UI with networking
 
-Show bets and raises in canvas
+    Show bets and raises in canvas
 
-Once it's fully functional, add animations, sounds, etc.
-Use require.js??
+    Once it's fully functional, add animations, sounds, etc.
+
+    Use require.js??
--- www/js/poker.js
+++ www/js/poker.js
@@ -20,8 +20,11 @@ function toSuit(c) {
     return c[1];
 }
 
-function scalingFactor (height, canvas) {
-    var factor = 6;
+var cardFactor = 6;
+
+var chipFactor = 15;
+
+function scale (height, canvas, factor) {
     return canvas.height/factor/height; 
 }
 
@@ -33,16 +36,16 @@ function cardHeight () {
     return 123;
 }
 
-function getCard (facedown, rank, suit, canvas) {
-    var cards = new Raster("cards");
-    cards.visible = false;
+function getCard (c, canvas) {
     var wd = cardWidth();
     var ht = cardHeight();
     var x,y;
-    if (facedown) {
+    if (c === "FD") {
         x = 2*wd;
         y = 4*ht;
     } else {
+        var rank = toRank(c);
+        var suit = toSuit(c);
         x = rank*wd;
         switch (suit) {
             case 'c':
@@ -60,19 +63,22 @@ function getCard (facedown, rank, suit, canvas) {
         }
     }
 
+    var cards = new Raster("cards");
+    cards.visible = false;
     var rect = new Rectangle(x,y,wd,ht);
     var card = cards.getSubRaster(rect);
-    card.scale(scalingFactor(cardHeight(), canvas));
+    cards.remove();
+    card.scale(scale(cardHeight(), canvas, cardFactor));
     return card;
 }
 
 
 function flop (cards, canvas) {
     var flop = cards.map(function (c) {
-        return getCard(false, toRank(c), toSuit(c), canvas);
+        return getCard(c, canvas);
     });
 
-    var width = cardWidth() * scalingFactor(cardHeight(), canvas);
+    var width = cardWidth() * scale(cardHeight(), canvas, cardFactor);
     for (var i=0; i<3; i++) {
         flop[i].position.x = view.center.x + width*(i-2);
         flop[i].position.y = view.center.y;
@@ -80,52 +86,84 @@ function flop (cards, canvas) {
 }
 
 function turn (c, canvas) {
-    var card = getCard(false, toRank(c), toSuit(c), canvas);
-    var width = cardWidth() * scalingFactor (cardHeight(), canvas);
+    var card = getCard(c, canvas);
+    var width = cardWidth() * scale (cardHeight(), canvas, cardFactor);
     card.position.x = view.center.x + 1.25*width;
     card.position.y = view.center.y;
 }
 
 function river (c, canvas) {
-    var card = getCard(false, toRank(c), toSuit(c), canvas);
-    var width = cardWidth() * scalingFactor (cardHeight(), canvas);
+    var card = getCard(c, canvas);
+    var width = cardWidth() * scale (cardHeight(), canvas, cardFactor);
     card.position.x = view.center.x + 2.5*width;
     card.position.y = view.center.y;
 }
 
-function player1Cards (facedown, cards, canvas) {
+function player1Cards (cards, canvas) {
     cards = cards.map(function (c) {
-        return getCard(facedown, toRank(c), toSuit(c), canvas);
+        return getCard(c, canvas);
     });
 
-    var width = cardWidth() * scalingFactor (cardHeight(), canvas);
+    var width = cardWidth() * scale (cardHeight(), canvas, cardFactor);
     for (var i=0; i<2; i++) {
         cards[i].position.x = view.center.x + width*(i/3-7);
         cards[i].position.y = view.center.y;
     }
 }
 
-function player2Cards (facedown, cards, canvas) {
+function player2Cards (cards, canvas) {
     cards = cards.map(function (c) {
-        return getCard(facedown, toRank(c), toSuit(c), canvas);
+        return getCard(c, canvas);
     });
 
-    var width = cardWidth() * scalingFactor (cardHeight(), canvas);
+    var width = cardWidth() * scale (cardHeight(), canvas, cardFactor);
     for (var i=0; i<2; i++) {
         cards[i].position.x = view.center.x - width*(i/3-7);
         cards[i].position.y = view.center.y;
     }
 }
 
-function player1Bet (amt, canvas) {
-    var x = view.center.x + 5*cardWidth() * scalingFactor(cardHeight(), canvas);
+function player1Bet (amt, canvas, list) {
+    var m = scale(cardHeight(), canvas, cardFactor);
+    var x = view.center.x + 5 * cardWidth() * m
     var y = view.center.y;
     var text = new PointText(new Point(x,y));
+    list.push(text);
     text.fillColor = 'black';
     text.content = '$'+amt;
+
+    var chip = new Raster("redChip");
+    list.push(chip);
+    var n = scale(chip.height, canvas, chipFactor);
+    chip.scale(n);
+    chip.position.x = view.center.x + 4 * cardWidth() * m;
+    chip.position.y = y;
 }
 
-window.onload = function () {
+function player2Bet (amt, canvas, list) {
+    var m = scale(cardHeight(), canvas, cardFactor);
+    var x = view.center.x - 5 * cardWidth() * m
+    var y = view.center.y;
+    var text = new PointText(new Point(x,y));
+    list.push(text);
+    text.fillColor = 'black';
+    text.content = '$'+amt;
+
+    var chip = new Raster("redChip");
+    list.push(chip);
+    var n = scale(chip.height, canvas, chipFactor);
+    chip.scale(n);
+    chip.position.x = view.center.x - 3.5 * cardWidth() * m;
+    chip.position.y = y;
+}
+
+function clearBets(L) {
+    L.map(function (p) {p.remove()});
+}
+
+window.onload = init;
+
+function init () {
     var canvas = document.getElementById("myCanvas");
     paper.setup(canvas);
     paper.install(window);
@@ -134,14 +172,18 @@ window.onload = function () {
     var tableBounds = new Rectangle(0,0, canvas.width, canvas.height); 
     table.fitBounds(tableBounds);
 
-    player1Cards(false, ['Kh', 'Kd'], canvas);
-    player2Cards(true, ['As', 'As'], canvas);
+    player1Cards(['Kh', 'Kd'], canvas);
+    player2Cards(['FD', 'FD'], canvas);
 
     flop(['As','Ac','Ad'], canvas);
     turn('Ah', canvas);
     river('Ks', canvas);
 
-    player1Bet(999, canvas);
+    var L = [];
+    player1Bet(999, canvas, L);
+    player2Bet(999, canvas, L);
+    clearBets(L);
+
 }
 
 
